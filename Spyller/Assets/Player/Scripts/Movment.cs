01f3ef9b2d081b2144ff2206c6ea88b7780816a4
xBiat <51037360+xBiat@users.noreply.github.com>
2020-09-20 01:41:58 -0700

    add c file

--- Spyller/Assets/Player/Scripts/Movment.cs
+++ Spyller/Assets/Player/Scripts/Movment.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Movment : MonoBehaviour
 {
-    public float speed = 6.0F;
-    public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
+    private float speed = 6.0F;
+    private float jumpSpeed = 8.0F;
+    private float gravity = 20.0F;
     private Vector3 moveDirection = Vector3.zero;
     private float turner;
-    private float looker;
-    public float sensitivity;
+    private float sensitivity = 5;
+    private Animator anim;
 
     int coin = 0;
     bool canThrow = false;
     void Start()
     {
         Cursor.visible = false;
+        anim = GetComponent<Animator>();
     }
 
    
@@ -27,31 +28,48 @@ public class Movment : MonoBehaviour
         if (controller.isGrounded)
         {
             
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             
            
             moveDirection *= speed;
             
             if (Input.GetButton("Jump"))
+            {
                 moveDirection.y = jumpSpeed;
-
+                anim.SetBool("is_in_air", true);
+            }
+            else
+            {
+                anim.SetBool("is_in_air", false);
+            }
+                
+            if (Input.GetKey(KeyCode.W))
+            {
+                anim.SetBool("run", true);
+            }
+            else
+            {
+                anim.SetBool("run", false);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                speed = 0;
+            }
+            else
+            {
+                speed = 6;
+            }
         }
         turner = Input.GetAxis("Mouse X") * sensitivity;
-        looker = -Input.GetAxis("Mouse Y") * sensitivity;
         if (turner != 0)
         {
             
             transform.eulerAngles += new Vector3(0, turner, 0);
         }
-        if (looker != 0)
-        {
-            
-            transform.eulerAngles += new Vector3(looker, 0, 0);
-        }
         
         moveDirection.y -= gravity * Time.deltaTime;
-        print("gg");
+        
         controller.Move(moveDirection * Time.deltaTime);
 
 
